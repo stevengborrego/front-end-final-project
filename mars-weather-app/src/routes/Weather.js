869da2e5cd00329128c83c64d600ca9ad8c94294
stevengborrego <sborrego@pdx.edu>
2023-05-19 14:46:59 -0700

    add support for .env api key storage

--- mars-weather-app/src/routes/Weather.js
+++ mars-weather-app/src/routes/Weather.js
@@ -1,4 +1,7 @@
 function Weather() {
+  const url = "https://api.nasa.gov/insight_weather/?api_key=" + process.env.REACT_APP_NASA_API_KEY + "&feedtype=json&ver=1.0"
+  console.log(url);
+
   return (
     <div className="container">
       <div className="row">
