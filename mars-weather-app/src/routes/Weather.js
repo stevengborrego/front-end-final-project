7b3d56f74fbd6b29aeb2a812bbb5168cef9267c1
stevengborrego <sborrego@pdx.edu>
2023-05-19 14:06:58 -0700

    add weather cards, .env file

--- mars-weather-app/src/routes/Weather.js
+++ mars-weather-app/src/routes/Weather.js
@@ -1,6 +1,38 @@
 function Weather() {
   return (
-    <p>Weather</p>
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Weather 1</h5>
+              <hr />
+              <p className="card-text">weather stuff</p>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Weather 2</h5>
+              <hr />
+              <p className="card-text">more weather stuff</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Weather 3</h5>
+              <hr />
+              <p className="card-text">weather stuff</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
