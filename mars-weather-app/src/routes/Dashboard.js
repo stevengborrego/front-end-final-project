53b4e529351b7615b156dd1902d7d6ffc624e554
stevengborrego <sborrego@pdx.edu>
2023-06-04 13:02:54 -0700

    implement space station on map

--- mars-weather-app/src/routes/Dashboard.js
+++ mars-weather-app/src/routes/Dashboard.js
@@ -1,73 +1,72 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
+import IssMap from "../IssMap.js";
 
-
-
-//const url = "https://api.nasa.gov/insight_weather/?api_key=" + process.env.REACT_APP_NASA_API_KEY + "&feedtype=json&ver=1.0";
-//const url = "";
-const weatherUrl = 'https://api.maas2.apollorion.com/';
-const issUrl = "http://api.open-notify.org/iss-now.json";
+const weatherUrl = "https://api.maas2.apollorion.com/";
 const peopleUrl = "http://api.open-notify.org/astros.json";
-
-function Weather(weather) {
-  return
-}
-
-function PeopleInSpace(people) {
-  return
-}
-
-function ISS(iss) {
-  return
-}
-
 
 function Dashboard() {
   const [weather, setWeather] = useState([]);
-  const [iss, setIss] = useState([]);
   const [people, setPeople] = useState([]);
 
   useEffect(() => {
-    Promise.all([
-      fetch(weatherUrl),
-      fetch(issUrl),
-      fetch(peopleUrl),
-    ])
-    .then(([resWeather, resISS, resPeople]) =>
-      Promise.all([resWeather.json(), resISS.json(), resPeople.json()])
-    )
-    .then(([dataWeather, dataISS, dataPeople]) => {
-      setWeather(dataWeather);
-      setIss(dataISS);
-      setPeople(dataPeople);
-    });
+    Promise.all([fetch(weatherUrl), fetch(peopleUrl)])
+      .then(([resWeather, resPeople]) =>
+        Promise.all([resWeather.json(), resPeople.json()])
+      )
+      .then(([dataWeather, dataPeople]) => {
+        setWeather(dataWeather);
+        setPeople(dataPeople);
+      });
   }, []);
 
-  // console.log(weather);
-  // console.log(people);
-  // console.log(iss);
-  // var longitude = iss.iss_position.longitude;
-  // var latitude = iss.iss_position.latitude;
+  console.log(weather);
 
   return (
     <div className="container">
       <div className="row">
-        <div className="col">
+        <div className="col-4">
           <div className="card">
             <div className="card-body">
               <h5 className="card-title">Mars Weather</h5>
               <hr />
-              <p className="card-text">Terrestrial Date: { weather.terrestrial_date }</p>
-              <p className="card-text">Max Temp: { weather.max_temp }</p>
+              <p className="card-text">
+                <b>Sol:</b> {weather.sol}
+              </p>
+              <p className="card-text">
+                <b>Terrestrial Date:</b> {weather.terrestrial_date}
+              </p>
+              <p className="card-text">
+                <b>Max Temp:</b> {weather.max_temp}&deg;C
+              </p>
+              <p className="card-text">
+                <b>Min Temp:</b> {weather.min_temp}&deg;C
+              </p>
             </div>
           </div>
         </div>
-        <div className="col">
+        <div className="col-8">
           <div className="card">
             <div className="card-body">
               <h5 className="card-title">People In Space</h5>
               <hr />
-              <p className="card-text">Number: { people.number }</p>
+              <div className="card-text">
+                {people.people && (
+                  <div className="peopleInSpace">
+                    {people.people.map((person) => {
+                      return (
+                        <div className="row" key={person.name}>
+                          <div className="col">
+                            <b>Name:</b> {person.name}
+                          </div>
+                          <div className="col">
+                            <b>Craft:</b> {person.craft}
+                          </div>
+                        </div>
+                      );
+                    })}
+                  </div>
+                )}
+              </div>
             </div>
           </div>
         </div>
@@ -78,8 +77,7 @@ function Dashboard() {
             <div className="card-body">
               <h5 className="card-title">ISS Tracker</h5>
               <hr />
-              { iss.iss_position && <p className="card-text">Latitude: { iss.iss_position.latitude }</p> }
-              { iss.iss_position && <p className="card-text">Longitude: { iss.iss_position.longitude }</p> }
+              <IssMap />
             </div>
           </div>
         </div>
